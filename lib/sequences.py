fba413663d5ea6a9d0badc7d19c54c8e8ef8da70
Elise Erickson <elisece717@gmail.com>
2024-02-02 17:25:21 -0500

    satisfies tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    printer = [0, 1]
+    for i in range(1, length - 1):
+        printer.append(printer[i - 1] + printer[i])
+    print(printer[0 : length])
+
+print_fibonacci(4)
